15e6d5bf2e9c75a3426ccd13a8d7c060f72940e5
Mohammed Safwan <safwanpklr@gmail.com>
2023-04-02 03:03:29 +0530

    created navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MovieHub';
+
+  navbg: any;
+
+  @HostListener('document:scroll') scrollover() {
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        'background-color': '#000000',
+      };
+    } else {
+      this.navbg = {
+        'background-color': '',
+      };
+    }
+  }
 }
