50cd7d1ebec0c7e4d7705a737b6e721cff2de0a7
Mohammed Safwan <safwanpklr@gmail.com>
2023-04-02 17:28:00 +0530

    banner and trending movies added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
 
+  bannerResult: any = [];
+  trendingMovieResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+    this.trendingData();
+  }
+
+  //bannerdata
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'banner result');
+      this.bannerResult = result.results;
+    });
+  }
+
+  //trendingdata
+  trendingData() {
+    this.service.trendingMovieApiData().subscribe((result) => {
+      console.log(result, 'trending result');
+      this.trendingMovieResult = result.results;
+    });
+  }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,4 +10,18 @@ export class MovieApiServiceService {
 
   baseurl = 'https://api.themoviedb.org/3';
   apikey = '08cc33bd5ae3a747598ce2ad84376e66';
+
+  //bannerapidata
+  bannerApiData(): Observable<any> {
+    return this.http.get(
+      `${this.baseurl}/trending/all/week?api_key=${this.apikey}`
+    );
+  }
+
+  // trendingmovieapidata
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(
+      `${this.baseurl}/trending/movie/day?api_key=${this.apikey}`
+    );
+  }
 }
